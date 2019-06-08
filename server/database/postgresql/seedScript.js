96bda89cab1198ee012407c60decb17888e8e71c
seo218 <casey.seo1@gmail.com>
2019-06-08 15:27:05 -0400

    descriptions and pictures seed update

--- server/database/postgresql/seedScript.js
+++ server/database/postgresql/seedScript.js
@@ -7,11 +7,14 @@ const db = new Pool({
   connectionString: connectionString,
 })
 
-const seedLimit = 100
+const seedLimit = 1000
+let insertSize = 10000
 let counter = 1
-let insertSize = 100000
+let projectCounter = counter
+let descriptionCounter = counter
+let picturesCounter = counter
 
-let rowGen = () => {
+let projectRowGen = () => {
   let data = ''
   let title, summary, video, address
   for(let i = 0; i < insertSize; i ++ ) {
@@ -32,54 +35,146 @@ let rowGen = () => {
   return data
 }
 
-console.time('seedTime');
+let descriptionRowGen = () => {
+  let data = ''
+  let description
+  for(let i = 0; i < insertSize; i ++ ) {
+    description = `'${faker.lorem.paragraph()}'`
+    if(i === insertSize -1) {
+      data = data.concat(`(${description})`)
+    } else {
+      data = data.concat(`(${description}), \n`)
+    }
+  }
+  return data
+}
 
-seedDb = () => {
+let picutresRowGen = () => {
+  let data = ''
+  let picture
+  for(let i = 0; i < insertSize; i ++ ) {
+    picture = `'${pictures[(Math.floor(Math.random() * pictures.length))]}'`
+    if(i === insertSize -1) {
+      data = data.concat(`(${picture})`)
+    } else {
+      data = data.concat(`(${picture}), \n`)
+    }
+  }
+  return data
+}
+
+seedProject = () => {
   return db.connect()
   .then((client) => {
-    if(counter < seedLimit) {
-      // console.log('if caught', counter)
+    if(projectCounter < seedLimit) {
       let insert = () => {
-        // console.log('insert ran', counter )
-        let data = rowGen()
-        // console.log(data)
+        let data = projectRowGen()
         let queryStr = 
-        `INSERT INTO project
-        (projectTitle, projectSummary, projectVideo, projectAddress)
+        `INSERT INTO project 
+        (projectTitle, projectSummary, projectVideo, projectAddress) 
         VALUES ${data}`
           return client
           .query(queryStr)
           .then(() => {
-            if(counter < seedLimit) {
-              // console.log('printing before', counter)
-              counter ++
-              console.log('printing counter', counter)
+            if(projectCounter < seedLimit) {
+              // console.log(`inserted into project table ${projectCounter} times`)
+              projectCounter ++
               return insert()
             } else {
-              return seedDb()
+              return seedProject()
             }
           })
           .catch((e) => {
-            // client.release()
             throw e
           })
         }
-      return insert()''
+      return insert()
     } else {
-      console.log('seeding complete')
+      console.log('product seed complete')
       return client.release()
     }
   })
 }
 
-return seedDb()
-.then(() => {
-  console.timeEnd('seedTime')
+seedDescription = () => {
+  return db.connect()
+  .then((client) => {
+    if(descriptionCounter < seedLimit) {
+      let insert = () => {
+        let data = descriptionRowGen()
+        let queryStr = 
+        `INSERT INTO descriptions
+        (descriptionEntry)
+        VALUES ${data}`
+          return client
+          .query(queryStr)
+          .then(() => {
+            if(descriptionCounter < seedLimit) {
+              // console.log(`inserted into description table ${descriptionCounter} times`)
+              descriptionCounter ++
+              return insert()
+            } else {
+              return seedDescription()
+            }
+          })
+          .catch((e) => {
+            throw e
+          })
+        }
+      return insert()
+    } else {
+      console.log('description seed complete')
+      return client.release()
+    }
   })
-.then(process.exit)
-.catch((e) => {
-  if(e) return console.log('failed to seed db =>', e)
-  process.exit
-})
+}
+
+seedPictures = () => {
+  return db.connect()
+  .then((client) => {
+    if(picturesCounter < seedLimit) {
+      let insert = () => {
+        let data = picutresRowGen()
+        let queryStr = `INSERT INTO pictures (pictureURL) VALUES ${data}`
+          return client
+          .query(queryStr)
+          .then(() => {
+            if(picturesCounter < seedLimit) {
+              // console.log(`inserted into pictures table ${picturesCounter} times`)
+              picturesCounter ++
+              return insert()
+            } else {
+              return seedPictures()
+            }
+          })
+          .catch((e) => {
+            throw e
+          })
+        }
+      return insert()
+    } else {
+      console.log('pictures seed complete')
+      return client.release()
+    }
+  })
+}
+
+console.time('seedTime');
+
+return seedProject()
+  .then(() => {
+    return seedDescription()  
+  })
+  .then(()=>{
+    return seedPictures()
+  })
+  .then(() => {
+    console.timeEnd('seedTime')
+    })
+  .then(process.exit)
+  .catch((e) => {
+    if(e) return console.log('failed to seed db =>', e)
+    process.exit
+  })
   
     

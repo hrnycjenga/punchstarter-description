10929310925af769ee9aad806d42e34d7472cac3
seo218 <casey.seo1@gmail.com>
2019-06-08 12:10:02 -0400

    db seeing time ~4 mins

--- server/database/postgresql/seedScript.js
+++ server/database/postgresql/seedScript.js
@@ -7,79 +7,79 @@ const db = new Pool({
   connectionString: connectionString,
 })
 
-const seedLimit = 100000000
-let counter = 0
-let insertSize = 10
-// 10000000000
+const seedLimit = 100
+let counter = 1
+let insertSize = 100000
+
 let rowGen = () => {
   let data = ''
   let title, summary, video, address
   for(let i = 0; i < insertSize; i ++ ) {
     title = `'${faker.commerce.productName()}'`
-    // console.log(typeof title, i)
     summary = `'${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()}'`
-    // console.log(summary, i)
     video =  `'${videos[(Math.floor(Math.random() * videos.length))]}'`
-    // console.log(video, i)
-    address = `'${faker.address.city(1)}, ${faker.address.stateAbbr()}'`
-    // console.log(address, i)
+    let city = faker.address.city(1)
+    while(city.includes("'")) {
+      city = faker.address.city(1)
+    }
+    address = `'${city}, ${faker.address.stateAbbr()}'`
     if(i === insertSize -1) {
-      // console.log('end of loop')
       data = data.concat(`(${title}, ${summary}, ${video}, ${address})`)
     } else {
-      data += data.concat(`(${title}, ${summary}, ${video}, ${address}), \n`)
+      data = data.concat(`(${title}, ${summary}, ${video}, ${address}), \n`)
     }
-    // console.log(data, i)
   }
   return data
 }
 
-// console.log('printing data outside of seedDB func => ' , rowGen().length)
 console.time('seedTime');
 
 seedDb = () => {
-  return db.connect().then((client) => {
-    let data = rowGen()
-    let queryStr = 
-    `INSERT INTO project
-    (projectTitle, projectSummary, projectVideo, projectAddress)
-     VALUES ${data}`
-    console.log('PRINTING query str', queryStr.length)
-    return client
-      .query(queryStr)
-      .then(() => {
-        return () => {
-          while(counter < seedLimit) {
-            // console.log(counter)
-            counter ++
-            // console.log(counter)
-            return seedDb()
-          }
+  return db.connect()
+  .then((client) => {
+    if(counter < seedLimit) {
+      // console.log('if caught', counter)
+      let insert = () => {
+        // console.log('insert ran', counter )
+        let data = rowGen()
+        // console.log(data)
+        let queryStr = 
+        `INSERT INTO project
+        (projectTitle, projectSummary, projectVideo, projectAddress)
+        VALUES ${data}`
+          return client
+          .query(queryStr)
+          .then(() => {
+            if(counter < seedLimit) {
+              // console.log('printing before', counter)
+              counter ++
+              console.log('printing counter', counter)
+              return insert()
+            } else {
+              return seedDb()
+            }
+          })
+          .catch((e) => {
+            // client.release()
+            throw e
+          })
         }
-      })
-      .catch((e) => {
-        client.release()
-        throw e
-      })
-      .then(() => {
-        console.log('ran')
-        client.release()
-        return console.log('seeding complete')
-      })
-      .catch((e) => {
-        client.release()
-        return console.log('failed to seed =>', e)
-      })
+      return insert()
+    } else {
+      console.log('seeding complete')
+      return client.release()
+    }
   })
 }
 
-// while (counter < seedLimit) {
-  return seedDb()
-  .then(process.exit, console.timeEnd('seedTime'))
-  .catch((e) => {
-    if(e) return console.log('failed to seed db =>', e)
-    process.exit
+return seedDb()
+.then(() => {
+  console.timeEnd('seedTime')
   })
+.then(process.exit)
+.catch((e) => {
+  if(e) return console.log('failed to seed db =>', e)
+  process.exit
+})
   
-// }
     

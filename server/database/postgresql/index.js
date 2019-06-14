05ba67e3a3d9f296b1a4550de210ad0b462592da
seo218 <casey.seo1@gmail.com>
2019-06-14 11:39:27 -0400

    postgres update

--- server/database/postgresql/index.js
+++ server/database/postgresql/index.js
@@ -1,5 +1,12 @@
 const { Pool } = require('pg')
-const connectionString = 'postgresql://postgres:1234@localhost:5432/postgres'
+const password = require('./password.js')
+const pgHost = process.env.PGHOST || 'localhost';
+const pgUser = process.env.PGUSER || 'punchcomments';
+const pgDatabase = process.env.PGDATABASE || 'postgres';
+const pgPassword = process.env.PGPASSWORD || password;
+const pgPort = process.env.PGPORT || 5432;
+const connectionString = `postgresql://${pgDatabase}:${pgPassword}@${pgHost}:${pgPort}/${pgUser}`
+
 
 const db = new Pool({
   connectionString: connectionString,
